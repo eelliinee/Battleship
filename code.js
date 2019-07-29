0b5758f11e9f0f5c9d014f04c917b2cdac2ba3e1
eelliinee <eja.bakker@gmail.com>
2019-07-29 12:24:35 +0200

    add fire and issunk method

--- code.js
+++ code.js
@@ -26,6 +26,34 @@ var model = {
 
     ships:  [{locations: ["06", "16", "26"], hits: ["", "", ""]},
             {locations: ["24", "34", "44"], hits: ["", "", ""]},
-            {locations: ["10", "11", "12"], hits: ["", "", ""]},]
+            {locations: ["10", "11", "12"], hits: ["", "", ""]}],
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            // determine if guess is in locations
+            var index = ship.locations.indexOf(guess);
+            // index is -1 if guess isn't in the array, so when we have a hit:
+            if (index >= 0) {
+                ship.hits[index]= "hit";
+                // sink ship when all locations are hit
+                if (this.isSunk(ship)) {
+                    this.shipsSunk++;
+                }
+                return true; 
+            }
+        return false;
+        }
+    },
+    isSunk: function(ship) {
+        //// book way
+        // for (var i = 0; i < this.shipLength; i++) {
+        //     if (ship.hits[i] !== "hit") {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        //// my way
+        ship.hits.includes("") ? false : true; 
+    }
 }
 
