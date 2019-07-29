5633ce228e9e9353c238c8a0f5697a911367d8cb
eelliinee <eja.bakker@gmail.com>
2019-07-29 11:51:56 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,31 @@
+var view = {
+    // displays string message in messageArea
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    
+    // place a miss in the grid
+    displayMissed: function(loc) {
+        var cell = document.getElementById(loc);
+        cell.setAttribute("class", "miss");
+    },
+
+    // place a hit in the grid
+    displayHit: function(loc) {
+        var cell = document.getElementById(loc);
+        cell.setAttribute("class", "hit");
+    }
+}
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships:  [{locations: ["06", "16", "26"], hits: ["", "", ""]},
+            {locations: ["24", "34", "44"], hits: ["", "", ""]},
+            {locations: ["10", "11", "12"], hits: ["", "", ""]},]
+}
+
