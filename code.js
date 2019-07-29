d23f02fe97da8dde702b2d267f19f47b690cb15a
eelliinee <eja.bakker@gmail.com>
2019-07-29 15:16:43 +0200

    complete function processGuess

--- code.js
+++ code.js
@@ -93,7 +93,14 @@ var controller = {
     guesses: 0,
 
     processGuess: function(guess) {
-
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
     }
 }
 
@@ -129,3 +136,18 @@ function parseGuess(guess) {
 
 // // test parseGuess
 //console.log(parseGuess("e6"));
+
+// // test controller
+// controller.processGuess("A0"); // miss
+
+// controller.processGuess("A6"); // hit
+// controller.processGuess("B6"); // hit
+// controller.processGuess("C6"); // hit
+
+// controller.processGuess("C4"); // hit
+// controller.processGuess("D4"); // hit
+// controller.processGuess("E4"); // hit
+
+// controller.processGuess("B0"); // hit
+// controller.processGuess("B1"); // hit
+// controller.processGuess("B2"); // hit
